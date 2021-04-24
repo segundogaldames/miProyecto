431df5e69535756ad4110a630360e247e07b30b7
Segundo Galdames <segundogaldames@gmail.com>
2021-04-24 11:20:48 -0300

    Implementacion de validaciones del formulario Personas

--- personas/add.php
+++ personas/add.php
@@ -22,39 +22,38 @@
 
     if (isset($_POST['confirm']) && $_POST['confirm'] == 1) {
         
-        //almacena el nombre de la region desde el formulario
+        //alamcenamos en variables la lista de campos que viene desde el formulario via POST
         $nombre = trim(strip_tags($_POST['nombre']));
+        $rut = trim(strip_tags($_POST['rut']));
+        $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+        $direccion = trim(strip_tags($_POST['direccion']));
+        $comuna = (int) $_POST['comuna'];
+        $fecha_nac = trim(strip_tags($_POST['fecha_nac']));
+        $telefono = (int) $_POST['telefono'];
+        $rol = (int) $_POST['rol'];
 
-        if (!$nombre) {
-            $msg = 'Debe ingresar el nombre de la región';
+        if (!$nombre || strlen($nombre) < 5) {
+            $msg = 'Ingrese el nombre de la persona';
+        }elseif(strlen($rut) < 9){
+            $msg = 'Ingrese el rut de la persona';
+        }elseif(!$email){
+            $msg = 'El email no es válido';
+        }elseif(!$direccion){
+            $msg = 'Ingrese la dirección de la persona';
+        }elseif ($comuna <= 0) {
+            $msg = 'Seleccione la comuna';
+        }elseif (strlen($telefono) < 9) {
+            $msg = 'El número de teléfono ingresado no es válido';
+        }elseif($rol <= 0){
+            $msg = 'Seleccione el rol';
         }else{
-
-            //preguntar si la region ingresada existe en la tabla regiones
-            $res = $mbd->prepare("SELECT id FROM regiones WHERE nombre = ?");
-            $res->bindParam(1, $nombre);
-            $res->execute();
-            $region = $res->fetch();
-
-            if ($region) {
-                $msg = 'La región ya existe... intente con otra';
-            }else{
-
-                //generamos una consulta con opciones de sanitizacion de datos
-                $res = $mbd->prepare("INSERT INTO regiones VALUES(null,?,now(),now())");
-                //validamos por cada signo de ? el dato que intentamos enviar a la base de datos
-                $res->bindParam(1, $nombre);
-                //se ejecuta la consulta de insercion de datos
-                $res->execute();
-
-                //pregunte si hubo registros ingresados
-                $row = $res->rowCount();
-
-                if ($row) {
-                    $msg = 'ok';
-                    header('Location: index.php?m=' . $msg);
-                }
-            }
+            //procedemos a verificar que el rut del usuario no exista
         }
+
+        /* echo '<pre>';
+        print_r($_POST);exit;
+        echo '</pre>'; */
+
     }
 
 ?>
@@ -65,7 +64,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Peraonas</title>
+    <title>Personas</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
 </head>
@@ -87,19 +86,19 @@
             <form action="" method="post">
                 <div class="form-group mb-3">
                     <label for="nombre">Nombre <span class="text-danger">*</span></label>
-                    <input type="text" name="nombre" class="form-control" placeholder="Ingrese nombre de la persona">
+                    <input type="text" name="nombre" value="<?php if(isset($_POST['nombre'])) echo $_POST['nombre']; ?>" class="form-control" placeholder="Ingrese nombre de la persona">
                 </div>
                 <div class="form-group mb-3">
                     <label for="rut">RUT <span class="text-danger">*</span></label>
-                    <input type="text" name="rut" class="form-control" placeholder="Ingrese RUT de la persona">
+                    <input type="text" name="rut" value="<?php if(isset($_POST['rut'])) echo $_POST['rut']; ?>" class="form-control" placeholder="Ingrese RUT de la persona">
                 </div>
                 <div class="form-group mb-3">
                     <label for="email">Email <span class="text-danger">*</span></label>
-                    <input type="email" name="email" class="form-control" placeholder="Ingrese email de la persona">
+                    <input type="email" name="email" value="<?php if(isset($_POST['email'])) echo $_POST['email']; ?>" class="form-control" placeholder="Ingrese email de la persona">
                 </div>
                 <div class="form-group mb-3">
                     <label for="direccion">Dirección <span class="text-danger">*</span></label>
-                    <input type="text" name="direccion" class="form-control" placeholder="Ingrese la dirección de la persona">
+                    <input type="text" name="direccion" value="<?php if(isset($_POST['direccion'])) echo $_POST['direccion']; ?>" class="form-control" placeholder="Ingrese la dirección de la persona">
                 </div>
                 <div class="form-group mb-3">
                     <label for="comuna">Comuna <span class="text-danger">*</span></label>
@@ -114,11 +113,11 @@
                 </div>
                 <div class="form-group mb-3">
                     <label for="fecha">Fecha de nacimiento (opcional)</label>
-                    <input type="date" name="fecha_nac" class="form-control" placeholder="Ingrese la fecha de nacimiento de la persona">
+                    <input type="date" name="fecha_nac" value="<?php if(isset($_POST['fecha_nac'])) echo $_POST['fecha_nac']; ?>" class="form-control" placeholder="Ingrese la fecha de nacimiento de la persona">
                 </div>
                 <div class="form-group mb-3">
                     <label for="telefono">Telefono (solo números)<span class="text-danger">*</span></label>
-                    <input type="number" name="telefono" class="form-control" placeholder="Ingrese el teléfono de la persona">
+                    <input type="number" name="telefono" value="<?php if(isset($_POST['telefono'])) echo $_POST['telefono']; ?>" class="form-control" placeholder="Ingrese el teléfono de la persona">
                 </div>
                 <div class="form-group mb-3">
                     <label for="rol">Rol <span class="text-danger">*</span></label>
